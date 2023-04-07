f7ac7c0afa4f6056d1bf66c0b8be5a2dabf126d0
BP2974 <mkbaek@the-51.com>
2023-04-07 15:13:43 +0900

    update

--- js/index.js
+++ js/index.js
@@ -5,16 +5,16 @@ $(document).ready(function(){
             $(".sec4 button").eq(i).addClass("active")
 
             if($(".sec4 button").eq(0).hasClass("active")){
-                $(".sec4 p").css({"font-style": "normal", "font-weight": "nromal"})
+                $(".sec4>p").css({"font-style": "normal", "font-weight": "nromal"})
             }
             else if($(".sec4 button").eq(1).hasClass("active")){
-                $(".sec4 p").css({"font-style": "italic", "font-weight": "normal"})
+                $(".sec4>p").css({"font-style": "italic", "font-weight": "normal"})
             }
             else if($(".sec4 button").eq(2).hasClass("active")){
-                $(".sec4 p").css({"font-style": "normal", "font-weight": "bold"})
+                $(".sec4>p").css({"font-style": "normal", "font-weight": "bold"})
             }
             else if($(".sec4 button").eq(3).hasClass("active")){
-                $(".sec4 p").css({"font-style": "italic", "font-weight": "bold"})
+                $(".sec4>p").css({"font-style": "italic", "font-weight": "bold"})
             }
         })
     }
